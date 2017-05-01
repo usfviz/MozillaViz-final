505a190f4b12d4792f6c75afb8e2cd35e2a25076
Connor Ameres <connorameres@gmail.com>
2017-05-01 15:38:17 -0700

    exclude_bower components (will replace with CDN when finalized)

--- project-prototype/app.R
+++ project-prototype/app.R
@@ -1,3 +1,16 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+list.of.packages <- c("D3partitionR", "jsonlite")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(shiny)
 library(D3partitionR)
 library(jsonlite)
